4145a32734855e39da02467d08f869a77383db64
Eric Krüger <ekrueger@codematix.de>
2021-03-25 09:37:43 +0100

    using environment for external urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://via-test.itz.res.bund.de/wsv/routing/api",
+  trafficUrl: "https://atlas.wsv.bund.de/elwis/rest/nts/messages/"
 };
